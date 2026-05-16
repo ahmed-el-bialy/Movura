51743b96df10229853606f8660d5ae87d93f61c0
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-05-16 15:35:07 +0300

    setup responsive UI with ScreenUtil and integrate HomeCubit provider

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movura/features/home_screen/data/repo/posters_repo.dart';
 
 import '../../features/details_screen/ui/details_screen.dart';
+import '../../features/home_screen/logic/main_content/main_content_cubit.dart';
 import '../../features/home_screen/ui/main_screen.dart';
 import '../helper/constants/strings.dart';
+import '../networking/di.dart';
 
 class AppRouter {
+  late PostersRepo postersRepo;
+  late MainContentCubit contentCubit;
+
+  AppRouter() {
+    postersRepo = sl<PostersRepo>();
+    contentCubit = MainContentCubit(postersRepo: postersRepo);
+  }
+
   Route generateRoute(RouteSettings setting) {
     switch (setting.name) {
       case Strings.mainScreen:
-        return MaterialPageRoute(builder: (_) => MainScreen());
+        return MaterialPageRoute(builder: (_) =>
+            BlocProvider(
+              create: (BuildContext context) => contentCubit,
+              child: MainScreen(),)
+        );
       case Strings.detailsScreen:
         return MaterialPageRoute(builder: (_) => DetailsScreen());
       default:
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movura/core/routing/app_router.dart';
 
 void main()async {
@@ -13,9 +14,17 @@ class Movura extends StatelessWidget {
 final AppRouter appRouter;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute:appRouter.generateRoute ,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          onGenerateRoute: appRouter.generateRoute,
+          debugShowCheckedModeBanner: false,
+          home: child,
+        );
+      },
     );
   }
 }
