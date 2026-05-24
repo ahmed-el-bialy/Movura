6ee0309509c2512f52c402d359f7106a1543f44f
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-05-24 21:29:35 +0300

    add ReviewCard widget to reviews tab in details screen

---
+++ lib/features/details_screen/ui/widgets/reviews_tab/review_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:movura/core/theming/colors.dart';
+
+class ReviewCard extends StatelessWidget {
+  const ReviewCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color:AppColors.onyxBlack,
+            borderRadius: BorderRadius.circular(16)
+      ),
+      child: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
