a0a7ae62c7522b60529fcee01d7111f1a44b34f6
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-06-06 18:52:25 +0300

    feat(tv_details): implement TV series feature layers and optimize core structures
    - Moved shared sub-models (Actors, Companies, Genres, Media) to the core layer for better reusability.
    - Restructured project directories by isolating Cubits and Web Services for each feature.
    - Updated Dependency Injection (di.dart) to register the newly added TvWebServices and TvSeriesAboutRepo.
    - Refactored screen routing to pass data cleanly as unified argument objects instead of raw parameters.
    - Implemented TvWebServices endpoints and full dynamic models for the TV Series Details screen.
    - Created the TV Details Cubit to manage state and fetch movie/TV details independently.
    - Performed overall code refactoring to clean up imports and enhance modularity.

--- lib/core/utils/constants/api_constants.dart
+++ lib/core/utils/constants/api_constants.dart
@@ -1,17 +1,17 @@
 class ApiConstants {
-  static const String companyImageError =
-      'https://imgs.search.brave.com/EJMCxyVRjJtDJDePd6-7kPazT9g-Q5_Xx5cUcKwVmhY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMjE1/NjQ1MDcwMy92ZWN0/b3IvNDA0LXBhZ2Ut/bm90LWZvdW5kLWlj/b24uanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPTlwRVFrbTRt/Y0x1Zmdzb1BhRkRp/cEsxNE1OSkV3aV9Q/QVZ1ZDBkaTV6aDg9';
-  static const String actorImageError =
-      'https://imgs.search.brave.com/N3LKytRAQg6sZ4CgMQ3XO5VPDYph_hzPT2ywP8CeGzk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzgzL2Jj/LzhiLzgzYmM4Yjg4/Y2Y2YmM0YjRlMDRk/MTUzYTQxOGNkZTYy/LmpwZw';
-  static const String videoCoverBaseUrl = 'https://img.youtube.com/vi/';
-
   static const String baseUrl = 'https://api.themoviedb.org/3/';
   static const String apiKey = '39f1bd360a83a7e31156a684be3f4350';
   static const String apiToken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOWYxYmQzNjBhODNhN2UzMTE1NmE2ODRiZTNmNDM1MCIsIm5iZiI6MTc3NzQ3NzExNi41NjksInN1YiI6IjY5ZjIyNWZjNzYyNmU3MThiMGRjZTA0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8URh-ciXG7o8iGzzS7AwY4vjO9JIBJiflPGNS6_otgE';
 
-  static const String imageBaseUrl = "https://image.tmdb.org/t/p/w500";
   static const String trendingMedia = "trending/all/day";
   static const String movieDetails = "movie";
   static const String tvDetails = "tv";
+  static const String imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+  static const String videoCoverBaseUrl = 'https://img.youtube.com/vi/';
+
+  static const String companyImageError =
+      'https://imgs.search.brave.com/EJMCxyVRjJtDJDePd6-7kPazT9g-Q5_Xx5cUcKwVmhY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMjE1/NjQ1MDcwMy92ZWN0/b3IvNDA0LXBhZ2Ut/bm90LWZvdW5kLWlj/b24uanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPTlwRVFrbTRt/Y0x1Zmdzb1BhRkRp/cEsxNE1OSkV3aV9Q/QVZ1ZDBkaTV6aDg9';
+  static const String actorImageError =
+      'https://imgs.search.brave.com/N3LKytRAQg6sZ4CgMQ3XO5VPDYph_hzPT2ywP8CeGzk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzgzL2Jj/LzhiLzgzYmM4Yjg4/Y2Y2YmM0YjRlMDRk/MTUzYTQxOGNkZTYy/LmpwZw';
 }
