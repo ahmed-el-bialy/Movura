bc961a6e37afc5bc6643f637508c4ac311776a2d
Ahmed El-Bialy <ah.elbialy.dev@gmail.com>
2026-06-20 15:50:59 +0300

    feat: Integrate Firebase configuration and dependencies for authentication and core functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movura/core/routing/app_router.dart';
+import 'package:movura/firebase_options.dart';
 
 import 'core/networking/di.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   await dotenv.load(fileName: ".env");
   await initDI();
+
   runApp(Movura(appRouter: AppRouter()));
 }
 
